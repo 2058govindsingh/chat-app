86798dd80337f6aafc2b548bef9d777e72d83f04
Govind Singh <govindyadav2058@gmail.com>
2023-12-04 21:57:07 +0530

    Connection Created

---
+++ index.js
@@ -0,0 +1,22 @@
+const http = require("http");
+const express = require("express");
+const { Server } = require("socket.io");
+const path = require("path");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Socket.io
+io.on("connection", (socket) => {
+  console.log("A new user has connected ", socket.id);
+});
+
+// Express
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", (req, res) => {
+  return res.sendFile("./public/index.html");
+});
+
+server.listen(9000, () => console.log(`Server started at PORT: 9000`));
